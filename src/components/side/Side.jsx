b057e53a4ca6913e0ba48deb2a0e6d11115a26db
HeebongYu <gmlqhd94@gmail.com>
2022-04-29 11:34:29 +0900

    2022.04.29-1

--- src/components/side/Side.jsx
+++ src/components/side/Side.jsx
@@ -4,15 +4,17 @@ import "./side.css";
 export default function Side() {
     return (
         <div className="side">
-            <iframe
-                title="ad"
-                src="https://ads-partners.coupang.com/widgets.html?id=574490&template=carousel&trackingCode=AF7105622&subId=&width=300&height=600"
-                width="300"
-                height="600"
-                frameBorder="0"
-                scrolling="no"
-                referrerPolicy="unsafe-url"
-            ></iframe>
+            <div className="ad">
+                <iframe
+                    title="ad"
+                    src="https://ads-partners.coupang.com/widgets.html?id=574490&template=carousel&trackingCode=AF7105622&subId=&width=300&height=600"
+                    width="300"
+                    height="600"
+                    frameBorder="0"
+                    scrolling="no"
+                    referrerPolicy="unsafe-url"
+                ></iframe>
+            </div>
         </div>
     );
 }
